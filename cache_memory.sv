b8975aee28c20bc79a2c60f37b24b893663f13f0
mhazizian <mohammadh.azizian@gmail.com>
2018-06-07 16:54:55 +0430

    Add cache module

---
+++ cache_memory.sv
@@ -0,0 +1,45 @@
+`include "defines.sv"
+
+module Cache_memory(clk, rst, address, miss, data_out);
+	
+	input [`ADDRESS_LEN - 1:0] address;
+	input clk, rst, miss;
+	output logic[`WORD_LEN - 1:0] data_out;
+
+	reg[`CACHE_BLOCK_LEN - 1:0] data[0:`CACHE_CAP - 1];
+
+	logic [`CACHE_INDEX_LEN - 1:0] idx;
+	assign idx = address[`ADDRESS_LEN - 2:3];
+
+	always @(posedge clk, rst) begin
+		if (rst) begin
+			data = '{default:8'b0};
+		end else if (miss) begin
+			data[idx][`CACHE_BLOCK_LEN - 1] = 0;
+
+		end else begin
+			if(data[idx][`CACHE_BLOCK_LEN - 1] == 1'b1) begin
+				if (data[idx][`CACHE_BLOCK_LEN - 2] == address[`ADDRESS_LEN - 1]) begin
+					case (address[1:0])
+						`2'b00 :
+							data_out = data[idx][31:0];
+						`2'b01 :
+							data_out = data[idx][63:32];
+						`2'b10 :
+							data_out = data[idx][95:64];
+						`2'b11 :
+							data_out = data[idx][127:96];
+					endcase // address[1:0]
+				end else
+					data[idx][`CACHE_BLOCK_LEN - 1] = 1'b1;
+			end 
+
+			if(data[idx][`CACHE_BLOCK_LEN - 1] == 1'b0) begin
+				// read from memory
+				
+				data[idx][`CACHE_BLOCK_LEN - 1] == 1'b1;
+			end
+		end 
+	end
+
+endmodule // Cache_memory
---
+++ defines.sv
@@ -0,0 +1,11 @@
+// LENGHTs
+
+`define ADDRESS_LEN 		15
+`define TAG_LEN				1
+`define WORD_LEN			32
+`define CACHE_INDEX_LEN		12
+
+`define CACHE_BLOCK_LEN		130
+`define CACHE_CAP			4096
+`define MEM_CAP				32768
+
